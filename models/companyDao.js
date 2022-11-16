23903443769a29601032efc0b677d23f5452eee6
Jisoo <k3ji0706@gmail.com>
2022-11-16 22:27:49 +0900

    ADJUST : Map함수로 에러 수정

--- models/companyDao.js
+++ models/companyDao.js
@@ -1,8 +1,9 @@
+const { json } = require('express');
 const myDataSource = require('../middlewares/typeorm')
 myDataSource.initialize()
 
 const company = async (companyId) => {
-  const companyPage = await myDataSource.query(`
+  let companyPage = await myDataSource.query(`
   SELECT
     company.id, company.company_name, ct.tags, ci.images, company.location
   FROM company
@@ -39,8 +40,15 @@ const company = async (companyId) => {
   WHERE id = '${companyId}'
   GROUP BY company.id
 `);
+  companyPage = [...companyPage].map(item => {
+    return {
+      ...item,
+      tags: JSON.parse(item.tags),
+      images: JSON.parse(item.images),
+    };
+  });
 
-  const companyPosts = await myDataSource.query(`
+  let companyPosts = await myDataSource.query(`
   SELECT
     posts.id, posts.due_date, posts.title, ps.tech_stacks, posts.company_id
   FROM posts
@@ -62,8 +70,14 @@ const company = async (companyId) => {
   ) ps ON posts.id = ps.posts_id
   WHERE company_id = '${companyId}'
 `)
+  companyPosts = [...companyPosts].map(item => {
+    return {
+      ...item,
+      tech_stacks: JSON.parse(item.tech_stacks),
+    };
+  });
 
-  const result = { companyPage, companyPosts }
+  let result = { companyPage, companyPosts }
   return result
 }
 
