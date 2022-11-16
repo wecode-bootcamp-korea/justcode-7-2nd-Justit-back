2e2055303b1cc611e329aa330fa81140fad8a79f
Jisoo <k3ji0706@gmail.com>
2022-11-16 21:08:13 +0900

    ADJUST : TYPEORM in middlewares

--- models/companyDao.js
+++ models/companyDao.js
@@ -1,12 +1,4 @@
-const { DataSource } = require('typeorm');
-const myDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-});
+const myDataSource = require('../middlewares/typeorm')
 myDataSource.initialize()
 
 const company = async (companyId) => {
