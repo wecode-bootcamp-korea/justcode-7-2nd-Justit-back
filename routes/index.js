04069b3c9c4f1a44e260b7ed7ebf55c990564495
김시아 <siakim@gimsiaui-MacBookAir.local>
2022-11-17 16:04:40 +0900

    FIX: resolve merge conflict

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,14 @@
 const express = require('express');
-<<<<<<< HEAD
 const router = express.Router();
 
-const userRouter = require('./user');
+const middleware = require('../middlewares/tokenError');
+const userRouter = require('./userRouter');
 const resumeRouter = require('./resumeRouter');
 const companyRouter = require('./companyRouter');
 
 router.use(userRouter);
-router.use('/resume', resumeRouter);
-=======
-const middleware = require('../middlewares/tokenError');
-const userRouter = require('./userRouter');
-const companyRouter = require('./companyRouter');
-
-const router = express.Router();
-router.use(userRouter);
 router.use(middleware.errorHandler);
->>>>>>> main
+router.use('/resume', resumeRouter);
 router.use('/company', companyRouter);
 
 module.exports = router;
