91293f84bf3dbecb92f1820fda47204cc85db291
Jisoo <k3ji0706@gmail.com>
2022-11-18 16:21:18 +0900

    ADD : 홈페이지 랜덤 포스트, 마감 임박순 API

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -3,12 +3,12 @@ const homePageService = require('../services/homePageService');
 const getHomePage = async (req, res) => {
   try {
       const result = await homePageService.getHomePage();
-      res.status(200).json(result)
+      res.status(200).json(result);
   } catch(err) {
     console.log(err);
-    res.status(err.statusCode).json({ message : err.message })
+    res.status(err.statusCode).json({ message : err.message });
   }
 
 }
 
-module.exports = { getHomePage }
+module.exports = { getHomePage };
--- services/homePageService.js
+++ services/homePageService.js
@@ -1,11 +1,16 @@
+const { useContainer } = require('typeorm');
 const homePageDao = require('../models/homePageDao');
 
 const getHomePage = async () => {
+  const randomPosts = await homePageDao.randomPosts();
+  const timeLimitPosts = await homePageDao.timeLimitPosts();
   const popularPosts = await homePageDao.popularPosts();
   const responseFastCompany = await homePageDao.responseFastCompany();
-  const newPosts = await homePageDao.newPosts()
-  const result = { popularPosts, responseFastCompany, newPosts }
-  return result
+  const newPosts = await homePageDao.newPosts();
+  const result = { randomPosts, timeLimitPosts, popularPosts, responseFastCompany, newPosts };
+  return result;
+
+
 }
 
-module.exports = { getHomePage }
+module.exports = { getHomePage };
