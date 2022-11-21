c02bde8d6d8fb036b046780c1d49ecdc56c8e39f
Jisoo <k3ji0706@gmail.com>
2022-11-22 00:11:06 +0900

    ADD : 필터 기능

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,14 +2,27 @@ const searchService = require('../services/searchService');
 
 const searchPosts = async (req, res) => {
   try {
-    const keyword = req.query.keyword
-    const tag = req.query.tag
-    const getPosts = await searchService.searchPosts(keyword, tag)
-    res.status(200).json(getPosts)
-  } catch (err) {
-    console.log(err)
-    res.status(err.statusCode).json({ message: err.message });
-  }
-}
+    const keyword = req.query.keyword || null;
+    const tag = req.query.tag || '';
+    const techStack = req.query.techStack || '';
+    const positionId = req.query.position || "''";
+    const location = req.query.location || null;
+    const career = req.query.career || null;
 
-module.exports = { searchPosts }
+    const REQUIRE_KEYS = [ keyword ];
+    REQUIRE_KEYS.map((key) => {
+      if (!key) {
+        throw new Error('검색어를 입력해주세요');
+      };
+    });
+
+    const getPosts =
+    await searchService.searchPosts(keyword, tag, techStack, positionId, location, career);
+    res.status(200).json(getPosts);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  };
+};
+
+module.exports = { searchPosts };
--- services/searchService.js
+++ services/searchService.js
@@ -1,39 +1,88 @@
 const searchDao = require('../models/searchDao');
+var _ = require('lodash');
 
-const searchPosts = async (keyword, tag) => {
-  if (keyword.length === 0) {
-    throw new Error('검색어를 입력해 주세요')
-  }
-  let tags = []
-  if (tag === undefined) {
-    let searchPosts = await searchDao.searchPosts(keyword)
-    return searchPosts
-  } else if (typeof (tag) == 'string') {
-    tags.push(`"${tag}"`)
+const searchPosts = async (keyword, tag, techStack, positionId, location, career) => {
+  const spe = /[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi;
+  if (spe.test(keyword)) {
+    throw new Error('특수문자는 사용할 수 없습니다.');
+  };
+
+  let tags = [];
+  if (typeof (tag) == 'string') {
+    tags.push(`"${tag}"`);
   } else if (tag !== undefined && tag.length > 1) {
     for (let i = 0; i < tag.length; i++) {
-      tags.push(`"${tag[i]}"`)
+      tags.push(`"${tag[i]}"`);
     }
   }
-  tags = tags.join(",")
-  let searchPosts = await searchDao.searchPosts(keyword)
-  const tagPosts = await searchDao.tagPosts(tags)
-
-
-
-
-  //   tag.forEach((v) => {
-  //   tags.push(`"${v}"`);
-  // })
-
-  console.log(tags)
-
-
-  if (searchPosts.length === 0) {
-    throw new Error('검색 결과가 없습니다')
+  if (tags.length > 1) {
+    tags = tags.join(",");
+  } else {
+    tags = tags[0];
   }
-  const result = { searchPosts, tagPosts }
-  return result
+
+  let techStacks = [];
+  if (typeof (techStack) == 'string') {
+    techStacks.push(`"${techStack}"`);
+  } else if (techStack !== undefined && techStack.length > 1) {
+    for (let i = 0; i < tag.length; i++) {
+      techStacks.push(`"${techStack[i]}"`);
+    };
+  };
+  if (techStacks.length > 1) {
+    techStacks = techStacks.join(",");
+  } else {
+    techStacks = techStacks[0];
+  };
+
+  let positionIds = [];
+  if (typeof (positionId) == 'string') {
+    positionIds.push(`"${techStack}"`);
+  } else if (positionId !== undefined && positionId.length > 1) {
+    for (let i = 0; i < tag.length; i++) {
+      positionIds.push(`"${techStack[i]}"`);
+    };
+  };
+  if (positionIds.length > 1) {
+    positionIds = positionIds.join(",");
+  } else {
+    positionIds = positionIds[0];
+  };
+
+  const searchPosts = await searchDao.searchPosts(keyword);
+  const tagPosts = await searchDao.tagPosts(tags);
+  const techStackPosts = await searchDao.techStackPosts(techStacks);
+  const positionPosts = await searchDao.positionPosts(positionId);
+  const locationPosts = await searchDao.locationPosts(location);
+  const careerPosts = await searchDao.careerPosts(career);
+
+  let filterPosts =
+    { tagPosts, techStackPosts, positionPosts, locationPosts, careerPosts };
+
+  filterPosts = filterPosts.tagPosts.concat(
+    filterPosts.techStackPosts, filterPosts.positionPosts,
+    filterPosts.locationPosts, filterPosts.careerPosts
+  );
+  filterPosts = _.uniqBy(filterPosts, 'postsId');
+
+  let result = []
+  if (filterPosts.length === 0) {
+    result = searchPosts
+  } else {
+    for (let i = 0; i < searchPosts.length; i++) {
+      for (let j = 0; j < filterPosts.length; j++) {
+        if (searchPosts[i].postsId === filterPosts[j].postsId) {
+          result.push(searchPosts[i]);
+        }
+      }
+    }
+  }
+
+  if (result.length === 0) {
+    throw new Error('검색 결과가 없습니다.');
+  }
+
+  return { result };
 }
 
-module.exports = { searchPosts }
+module.exports = { searchPosts };
