f1d2b4c14a884a88e5640bac015bbccbd49da9ab
Jisoo <k3ji0706@gmail.com>
2022-11-23 11:35:27 +0900

    FIX : techStack, positionId 오타 수정

--- services/searchService.js
+++ services/searchService.js
@@ -25,7 +25,7 @@ const searchPosts = async (keyword, tag, techStack, positionId, location, career
   if (typeof (techStack) == 'string') {
     techStacks.push(`"${techStack}"`);
   } else if (techStack !== undefined && techStack.length > 1) {
-    for (let i = 0; i < tag.length; i++) {
+    for (let i = 0; i < techStack.length; i++) {
       techStacks.push(`"${techStack[i]}"`);
     };
   };
@@ -37,10 +37,10 @@ const searchPosts = async (keyword, tag, techStack, positionId, location, career
 
   let positionIds = [];
   if (typeof (positionId) == 'string') {
-    positionIds.push(`"${techStack}"`);
+    positionIds.push(`"${positionId}"`);
   } else if (positionId !== undefined && positionId.length > 1) {
-    for (let i = 0; i < tag.length; i++) {
-      positionIds.push(`"${techStack[i]}"`);
+    for (let i = 0; i < positionId.length; i++) {
+      positionIds.push(`"${positionId[i]}"`);
     };
   };
   if (positionIds.length > 1) {
