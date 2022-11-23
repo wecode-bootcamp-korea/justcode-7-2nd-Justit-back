e588d6b7aae2477eec1b1657a0d5ca9e71595ef9
Jisoo <k3ji0706@gmail.com>
2022-11-23 11:11:23 +0900

    FIX : 검색페이지 필터 기능

--- models/searchDao.js
+++ models/searchDao.js
@@ -4,34 +4,33 @@ const searchPosts = async keyword => {
   let searchPost = await myDataSource.query(`
   SELECT
     posts.id as postsId, co.images, co.company_name, posts.title, ps.tech_stacks,
-    co.location, posts.career_min, career_max, view, posts.position_id,
-    posts.company_id
+    co.location, posts.career_min, career_max, view, posts.position_id
   FROM posts
-    LEFT JOIN (
-        SELECT
-      posts_id,
-      JSON_ARRAYAGG(
-        JSON_OBJECT(
-        "id", tech_stack.id,
-        "tech_stack", tech_stack.tech_stack_name
-        )
-      ) as tech_stacks
-      FROM
-        posts_tech_stack
-      JOIN
-        tech_stack ON posts_tech_stack.tech_stack_id = tech_stack.id
-      GROUP BY
-        posts_id
-    ) ps ON posts.id = ps.posts_id
-    LEFT JOIN education
-    ON education.id = posts.education_id
-    LEFT JOIN (
+  LEFT JOIN (
     SELECT
-      company.id, company.company_name, ct.tags, ci.images, company.location
+      posts_id,
+    JSON_ARRAYAGG(
+      JSON_OBJECT(
+      "id", posts_tech_stack.id,
+      "tech_stack", tech_stack.tech_stack_name
+      )
+    ) as tech_stacks
+    FROM
+      posts_tech_stack
+    JOIN
+      tech_stack ON posts_tech_stack.tech_stack_id = tech_stack.id
+    GROUP BY
+      posts_id
+  ) ps ON posts.id = ps.posts_id
+  LEFT JOIN education
+  ON education.id = posts.education_id
+  LEFT JOIN (
+    SELECT
+      company.id, company.company_name, ci.images, company.location
     FROM company
     LEFT JOIN (
       SELECT
-      company_id,
+        company_id,
       JSON_ARRAYAGG(
         JSON_OBJECT(
         "id", id,
@@ -39,29 +38,13 @@ const searchPosts = async keyword => {
         )
       ) as images
       FROM
-          image
-      GROUP BY
-          company_id
-    ) as ci ON company.id = ci.company_id
-    LEFT JOIN (
-      SELECT
-      company_id,
-      JSON_ARRAYAGG(
-        JSON_OBJECT(
-        "id", tag.id,
-        "tag", tag.tag_name
-        )
-      ) as tags
-      FROM
-        company_tag
-      JOIN
-        tag ON company_tag.tag_id = tag.id
+        image
       GROUP BY
         company_id
-    ) ct ON company.id = ct.company_id
+    ) as ci ON company.id = ci.company_id
   )as co ON co.id = posts.company_id
   WHERE (content LIKE '%${keyword}%' or title LIKE '%${keyword}%')
-  `);
+`);
 
   searchPost = [...searchPost].map(item => {
     return {
@@ -77,66 +60,66 @@ const searchPosts = async keyword => {
 const tagPosts = async tags => {
   let tagPost = await myDataSource.query(`
   SELECT
-    posts.id as postsId, co.images, co.company_name, posts.title, ps.tech_stacks, co.location, posts.career_min, career_max, view, posts.position_id, posts.company_id,
-    posts.due_date
+    posts.id as postsId, co.images, co.company_name, posts.title, ps.tech_stacks,
+    co.location, posts.career_min, career_max, view, posts.position_id
   FROM posts
-    LEFT JOIN (
-        SELECT
-        posts_id,
-        JSON_ARRAYAGG(
-        JSON_OBJECT(
-          "id", tech_stack.id,
-          "tech_stack", tech_stack.tech_stack_name
-          )
-        ) as tech_stacks
-        FROM
-          posts_tech_stack
-        JOIN
-          tech_stack ON posts_tech_stack.tech_stack_id = tech_stack.id
-        GROUP BY
-          posts_id
-      ) ps ON posts.id = ps.posts_id
-    LEFT JOIN education
-    ON education.id = posts.education_id
+  LEFT JOIN (
+    SELECT
+      posts_id,
+    JSON_ARRAYAGG(
+      JSON_OBJECT(
+      "id", tech_stack.id,
+      "tech_stack", tech_stack.tech_stack_name
+        )
+      ) as tech_stacks
+    FROM
+      posts_tech_stack
+    JOIN
+      tech_stack ON posts_tech_stack.tech_stack_id = tech_stack.id
+    GROUP BY
+        posts_id
+    ) ps ON posts.id = ps.posts_id
+  LEFT JOIN education
+  ON education.id = posts.education_id
+  LEFT JOIN (
+    SELECT
+      company.id, company.company_name, ct.tags, ci.images, company.location
+    FROM company
     LEFT JOIN (
       SELECT
-      company.id, company.company_name, ct.tags, ci.images, company.location
-      FROM company
-        LEFT JOIN (
-          SELECT
-          company_id,
-          JSON_ARRAYAGG(
-            JSON_OBJECT(
-            "id", id,
-            "image", image
-            )
-          ) as images
-          FROM
-              image
-          GROUP BY
-              company_id
-        ) as ci ON company.id = ci.company_id
-        LEFT JOIN (
-                SELECT
-          company_id,
-          JSON_ARRAYAGG(
-            JSON_OBJECT(
-            "id", tag.id,
-              "tag", tag.tag_name
-            )
-          ) as tags
-          FROM
-              company_tag
-          JOIN
-            tag ON company_tag.tag_id = tag.id
-            WHERE tag.tag_name IN (${tags})
-          GROUP BY
-              company_id
-              HAVING COUNT(tag_id) >= '1'
-      ) ct ON company.id = ct.company_id
-    )as co ON co.id = posts.company_id
-    WHERE NOT tags is NULL
-    `)
+        company_id,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+        "id", id,
+        "image", image
+        )
+      ) as images
+      FROM
+        image
+      GROUP BY
+        company_id
+    ) as ci ON company.id = ci.company_id
+    LEFT JOIN (
+      SELECT
+        company_id,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+        "id", tag.id,
+        "tag", tag.tag_name
+        )
+      ) as tags
+      FROM
+        company_tag
+      JOIN
+        tag ON company_tag.tag_id = tag.id
+      WHERE tag.tag_name IN (${tags})
+      GROUP BY
+        company_id
+      HAVING COUNT(tag_id) >= '1'
+    ) ct ON company.id = ct.company_id
+  )as co ON co.id = posts.company_id
+  WHERE NOT tags is NULL
+`)
 
   tagPost = [...tagPost].map(item => {
     return {
@@ -194,8 +177,8 @@ const techStackPosts = async techStacks => {
         company_id
     ) as ci ON company.id = ci.company_id
   )as co ON co.id = posts.company_id
-WHERE NOT tech_stacks is NULL
-  `);
+  WHERE NOT tech_stacks is NULL
+`);
 
   techStackPost = [...techStackPost].map(item => {
     return {
@@ -251,7 +234,7 @@ const positionPosts = async positionId => {
         company_id
     ) as ci ON company.id = ci.company_id
   )as co ON co.id = posts.company_id
-WHERE position_id IN (${positionId})
+  WHERE position_id IN (${positionId})
 `);
 
   positionPost = [...positionPost].map(item => {
@@ -308,7 +291,7 @@ const locationPosts = async location => {
         company_id
     ) as ci ON company.id = ci.company_id
   )as co ON co.id = posts.company_id
-WHERE location LIKE '%${location}%';
+  WHERE location LIKE '%${location}%';
 `);
 
   locationPost = [...locationPost].map(item => {
@@ -365,7 +348,7 @@ const careerPosts = async career => {
         company_id
     ) as ci ON company.id = ci.company_id
   )as co ON co.id = posts.company_id
-WHERE career_min < ${career} < career_max
+  HAVING career_min <= ${career} AND ${career} <= career_max
 `);
 
   careerPost = [...careerPost].map(item => {
--- services/searchService.js
+++ services/searchService.js
@@ -65,22 +65,78 @@ const searchPosts = async (keyword, tag, techStack, positionId, location, career
   );
   filterPosts = _.uniqBy(filterPosts, 'postsId');
 
-  let result = []
-  if (filterPosts.length === 0) {
-    result = searchPosts
-  } else {
-    for (let i = 0; i < searchPosts.length; i++) {
-      for (let j = 0; j < filterPosts.length; j++) {
-        if (searchPosts[i].postsId === filterPosts[j].postsId) {
-          result.push(searchPosts[i]);
+  let tagAndTechStackPost = [];
+  if (tagPosts.length !== 0 && techStackPosts.length !== 0) {
+    for (let i = 0; i < tagPosts.length; i++) {
+      for (let j = 0; j < techStackPosts.length; j++) {
+        if (tagPosts[i].postsId === techStackPosts[j].postsId) {
+          tagAndTechStackPost.push(tagPosts[i]);
         }
       }
     }
-  }
+  };
+
+  const strSearchPosts = searchPosts.map(JSON.stringify);
+  const strPositionPosts = positionPosts.map(JSON.stringify);
+  const strLocationPosts = locationPosts.map(JSON.stringify);
+  const strCareerPosts = careerPosts.map(JSON.stringify);
+
+  let anotherPost = strSearchPosts;
+
+  if (strPositionPosts.length !== 0) {
+    anotherPost = anotherPost.filter(el => strPositionPosts.includes(el));
+  };
+  if (strLocationPosts.length !== 0) {
+    anotherPost = anotherPost.filter(el => strLocationPosts.includes(el));
+  };
+  if (strCareerPosts.length !== 0) {
+    anotherPost = anotherPost.filter(el => strCareerPosts.includes(el));
+  };
+  anotherPost = anotherPost.map(JSON.parse);
+
+  let result = [];
+
+  if (tagAndTechStackPost.length !== 0) {
+    for (let i = 0; i < anotherPost.length; i++) {
+      for (let j = 0; j < tagAndTechStackPost.length; j++) {
+        if (anotherPost[i].postsId === tagAndTechStackPost[j].postsId) {
+          result.push(tagAndTechStackPost[j]);
+        }
+      }
+    }
+  };
+
+  if (techStackPosts.length === 0 && tagPosts.length !== 0) {
+    for (let i = 0; i < anotherPost.length; i++) {
+      for (let j = 0; j < tagPosts.length; j++) {
+        if (anotherPost[i].postsId === tagPosts[j].postsId) {
+          result.push(tagPosts[j]);
+        }
+      }
+    }
+  };
+
+  if (techStackPosts.length !== 0 && tagPosts.length === 0){
+    for (let i = 0; i < anotherPost.length; i++) {
+      for (let j = 0; j < techStackPosts.length; j++) {
+        if (anotherPost[i].postsId === techStackPosts[j].postsId) {
+          result.push(techStackPosts[j]);
+        }
+      }
+    }
+  };
+
+  if (techStackPosts.length === 0 && tagPosts.length === 0) {
+    result = anotherPost;
+  };
 
   if (result.length === 0) {
-    throw new Error('검색 결과가 없습니다.');
-  }
+    throw new Error('게시글이 없습니다.');
+  };
+
+  if (filterPosts.length === 0) {
+    result = searchPosts
+  };
 
   return { result };
 }
