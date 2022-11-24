14769270eb8199f9de73eb7d583500ef9b9578d8
이우령 <wooryeongg@gmail.com>
2022-11-24 22:21:40 +0900

    fix: 포트 수정

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { createApp } = require('./app');
 
 const startServer = async () => {
   const app = createApp();
-  const PORT = process.env.PORT || 8001;
+  const PORT = process.env.PORT || 8000;
 
   app.listen(PORT, () => {
     console.log(`server start : http://localhost:${PORT}/`);
