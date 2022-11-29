baab3876df5465425566a0346925792bf796ed7e
이우령 <wooryeongg@gmail.com>
2022-11-29 17:58:22 +0900

    fix: 라우터 이름 변경

--- controllers/scrapController.js
+++ controllers/scrapController.js
@@ -1,4 +1,4 @@
-const mypageService = require('../services/scrapService');
+const scrapService = require('../services/scrapService');
 const utils = require('../utils/myutils');
 
 // 스크랩 추가
@@ -7,7 +7,7 @@ const addPostScrap = async (req, res) => {
   const userId = req.userInfo.id;
   utils.checkDataIsNotEmpty({ posts_id, userId });
 
-  await mypageService.addPostScrap(posts_id, userId);
+  await scrapService.addPostScrap(posts_id, userId);
 
   res.status(201).json({ message: 'SCRAP_SUCCESS' });
 };
@@ -17,7 +17,7 @@ const findPostByUserId = async (req, res) => {
   const userId = req.userInfo.id;
   utils.checkDataIsNotEmpty({ userId });
 
-  const findPost = await mypageService.findPostByUserId(userId);
+  const findPost = await scrapService.findPostByUserId(userId);
 
   res.status(200).json({ message: 'FIND_SCRAP_LIST', data: findPost });
 };
@@ -28,7 +28,7 @@ const deletePost = async (req, res) => {
   const userId = req.userInfo.id;
   utils.checkDataIsNotEmpty({ posts_id, userId });
 
-  await mypageService.deletePost(posts_id, userId);
+  await scrapService.deletePost(posts_id, userId);
 
   res.status(201).json({ message: 'DELETE_SCRAP_SUCCESSFULLY' });
 };
--- services/scrapService.js
+++ services/scrapService.js
@@ -1,20 +1,20 @@
-const mypageDao = require('../models/scrapDao');
+const scrapDao = require('../models/scrapDao');
 
 // 스크랩 추가
 const addPostScrap = async (posts_id, userId) => {
-  const addPost = await mypageDao.addPostScrap(posts_id, userId);
+  const addPost = await scrapDao.addPostScrap(posts_id, userId);
   return addPost;
 };
 
 // 스크랩 보기
 const findPostByUserId = async userId => {
-  const findPost = await mypageDao.findPostByUserId(userId);
+  const findPost = await scrapDao.findPostByUserId(userId);
   return findPost;
 };
 
 // 스크랩 삭제
 const deletePost = async (posts_id, userId) => {
-  const deletePost = await mypageDao.deletePost(posts_id, userId);
+  const deletePost = await scrapDao.deletePost(posts_id, userId);
   return deletePost;
 };
 
